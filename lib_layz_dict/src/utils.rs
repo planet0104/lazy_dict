498bb742c2921bf49c45a65f582c12f1cfbf5b27
Jia Ye <planet0104@gmail.com>
2018-10-20 22:14:09 +0800

    preview ok.

--- lib_layz_dict/src/utils.rs
+++ lib_layz_dict/src/utils.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::io::Read;
 use zip;
 use std::io::{Result, Error, ErrorKind};
+use rayon;
 
 // 获取包名
 pub fn get_package_name() -> String{
@@ -147,4 +148,161 @@ pub fn copy_assets(filename:&str) -> Result<String>{
 use std::time::{Duration};
 pub fn duration_to_milis(duration: &Duration) -> f64 {
     duration.as_secs() as f64 * 1000.0 + duration.subsec_nanos() as f64 / 1_000_000.0
+}
+
+
+// This value is 2 ^ 18 - 1, and is used to clamp the RGB values before their
+// ranges
+// are normalized to eight bits.
+const K_MAX_CHANNEL_VALUE:i32 = 262143;
+///https://github.com/xvolica/Camera2-Yuv2Rgb/blob/master/jni/yuv2rgb.cpp
+fn yuv_to_rgb(mut y:i32, mut u:i32, mut v:i32) -> (u8,u8,u8){
+	use std::cmp::{min, max};
+
+	y -= 16;
+	u -= 128;
+	v -= 128;
+	if y<0{
+		y=0;
+	}
+
+	let mut r = 1192 * y + 1634 * v;
+	let mut g = 1192 * y - 833 * v - 400 * u;
+	let mut b = 1192 * y + 2066 * u;
+
+	r = min(K_MAX_CHANNEL_VALUE, max(0, r));
+	g = min(K_MAX_CHANNEL_VALUE, max(0, g));
+	b = min(K_MAX_CHANNEL_VALUE, max(0, b));
+
+	r = (r>>10) & 0xff;
+	g = (g>>10) & 0xff;
+	b = (b>>10) & 0xff;
+
+	//0xff000000 | (r as u32) << 24 | (g as u32) << 16 | (b as u32)<<8
+	(r as u8, g as u8, b as u8)
+}
+
+pub fn yuv_420_to_rgb_888(y_data: &[u8], u_data: &[u8], v_data: &[u8], output:&mut[u8], width: i32, height:i32, y_row_stride: i32, uv_row_stride:i32, uv_pixel_stride:i32){
+
+	let multi_thread = true;
+
+	if multi_thread{ //多线程 平均5ms (图片640x480)
+		//每行一个线程执行
+		// rayon::scope(|s| {
+		// 	for (j, output_slice) in output.chunks_mut(width as usize*3).enumerate() {
+		// 		s.spawn(move |_| {
+		// 			let y = j as i32;
+		// 			let iy = y_row_stride*y;
+		// 			let uv_row_start = uv_row_stride*(y>>1);
+		// 			let iu = uv_row_start;
+		// 			let iv = uv_row_start;
+		// 			let mut iout = 0;
+		// 			for x in 0..width{
+		// 				let uv_offset = (x>>1)*uv_pixel_stride;
+		// 				let (r, g, b) = yuv_to_rgb(y_data[(iy+x) as usize] as i32, u_data[(iu+uv_offset) as usize] as i32, v_data[(iv+uv_offset) as usize] as i32);
+		// 				output_slice[iout] = r; iout+=1;
+		// 				output_slice[iout] = g; iout+=1;
+		// 				output_slice[iout] = b; iout+=1;
+		// 			}
+		// 		});
+		// 	}
+		// });
+
+		rayon::scope(|s| {
+			let mut i = 0;
+			//检查height的整除数 分成10个以内线程去执行
+			let mut step = 1;
+			for h in (2..11).rev().step_by(2){
+				if height%h==0{
+					step = (height/h) as usize;
+					break;
+				}
+			}
+			for (j, output_slice) in output.chunks_mut(width as usize*3*step).enumerate() {
+				s.spawn(move |_| {
+					let mut iout = 0;
+					for ayi in 0..step{
+						let y = (j+i+ayi) as i32;
+						let iy = y_row_stride*y;
+						let uv_row_start = uv_row_stride*(y>>1);
+						let iu = uv_row_start;
+						let iv = uv_row_start;
+						for x in 0..width{
+							let uv_offset = (x>>1)*uv_pixel_stride;
+							let (r, g, b) = yuv_to_rgb(y_data[(iy+x) as usize] as i32, u_data[(iu+uv_offset) as usize] as i32, v_data[(iv+uv_offset) as usize] as i32);
+							output_slice[iout] = r; iout+=1;
+							output_slice[iout] = g; iout+=1;
+							output_slice[iout] = b; iout+=1;
+						}
+					}
+				});
+				i += step-1;
+			}
+		});
+	}else{
+		//单线程 约11ms
+		let mut iout = 0;
+		for y in 0..height{
+			let iy = y_row_stride*y;
+			let uv_row_start = uv_row_stride*(y>>1);
+			let iu = uv_row_start;
+			let iv = uv_row_start;
+			for x in 0..width{
+				let uv_offset = (x>>1)*uv_pixel_stride;
+				let (r, g, b) = yuv_to_rgb(y_data[(iy+x) as usize] as i32, u_data[(iu+uv_offset) as usize] as i32, v_data[(iv+uv_offset) as usize] as i32);
+				output[iout] = r; iout+=1;
+				output[iout] = g; iout+=1;
+				output[iout] = b; iout+=1;
+			}
+		}
+	}
+}
+
+//RGB顺时针旋转90度
+pub fn rotate90(src_buffer: &[u8], new_buffer:&mut [u8], src_width: usize, src_height:usize) -> (usize, usize){
+    let (new_width, new_height) = (src_height, src_width);
+    for (y, row) in src_buffer.chunks(src_width*3).enumerate(){
+        //tx = src_height-y-1;
+        //ty = sx;
+        let n = (src_height-y-1)*3;
+        for (x, pixel) in row.chunks(3).enumerate(){
+            let p = x*new_width*3+n;
+            new_buffer[p] = pixel[0];
+            new_buffer[p+1] = pixel[1];
+            new_buffer[p+2] = pixel[2];
+        }
+    }
+    (new_width, new_height)
+}
+
+//RGB顺时针旋转180度
+pub fn rotate180(src_buffer:&[u8], new_buffer:&mut [u8], width: usize, height: usize) -> (usize, usize){
+    let stride = width*3;
+    let mut p = src_buffer.len()-1;
+    for row in src_buffer.chunks(stride){
+        for pixel in row.chunks(3){
+            new_buffer[p-2] = pixel[0];
+            new_buffer[p-1] = pixel[1];
+            new_buffer[p] = pixel[2];
+            p -= 3;
+        }
+    }
+    (width, height)
+}
+
+//RGB顺时针旋转270度
+pub fn rotate270(src_buffer: &[u8], new_buffer:&mut [u8], src_width: usize, src_height:usize) -> (usize, usize){
+    let (new_width, new_height) = (src_height, src_width);
+    let src_stride = src_width*3;
+    let new_stride = new_width*3;
+    for (y, row) in src_buffer.chunks(src_stride).enumerate(){
+        let j = y*3;
+        for (x, pixel) in row.chunks(3).enumerate(){
+            let p = (src_width-x-1)*new_stride+j;
+            new_buffer[p] = pixel[0];
+            new_buffer[p+1] = pixel[1];
+            new_buffer[p+2] = pixel[2];
+        }
+    }
+    (new_width, new_height)
 }
