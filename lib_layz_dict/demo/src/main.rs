9aa667be550fa6e58f2fbca79265e513b6c08fda
planet0104 <planet0104@gmail.com>
2018-10-25 18:08:10 +0800

    t

--- lib_layz_dict/demo/src/main.rs
+++ lib_layz_dict/demo/src/main.rs
@@ -1,7 +1,7 @@
 extern crate image;
 use std::time::{Duration, Instant};
-mod retina;
-use image::{ImageBuffer, Rgb};
+use image::{GenericImage, Rgb, ImageBuffer};
+extern crate imageproc;
 
 /// 双峰直方图计算阈值
 /// 
@@ -10,25 +10,28 @@ use image::{ImageBuffer, Rgb};
 /// # Params
 /// - `pixels`: 图像数据
 /// - `bpp`: 每个像素占用字节 
-fn calc_threshold(pixels: &[u8], bpp: usize) -> u8{
+/// 
+/// # Return
+/// - (u8, Vec<u8>) (阈值, 每个像素对应灰度)
+fn calc_threshold(pixels: &[u8], bpp: usize) -> (u8, Vec<u8>){
     //统计灰度直方图数据
     let mut gray_count = vec![0; 256];
     let mut gray_sum = 0;
     let pixel_count = pixels.len()/bpp;//总像素数
-    let mut gray_values = vec![0; pixel_count];
+    let mut gray_values = vec![0u8; pixel_count];
     //循环每个像素统计灰度总和和灰度平均值
     for (i, pixel) in pixels.chunks(bpp).enumerate(){
         let gray =  (77*(pixel[0] as usize) + 150*(pixel[1] as usize) + 29*(pixel[2] as usize)+ 128) >> 8;
         gray_count[gray] += 1;
         gray_sum += gray;
-        gray_values[i] = gray as i32;
+        gray_values[i] = gray as u8;
     }
 
     // (1) 计算图像灰度平均值、标准偏差(标准差)sigma
     let avg = (gray_sum as f32/pixel_count as f32) as i32;//计算灰度平均值
     let sigma = {//计算标准差
         //方差和
-        let total:i32 = gray_values.iter().map(|v|{ (*v-avg)*(*v-avg) }).sum();
+        let total:i32 = gray_values.iter().map(|v|{ (*v as i32-avg)*(*v as i32-avg) }).sum();
         (total as f32/pixel_count as f32).sqrt()//求出标准差
     };
 
@@ -40,83 +43,143 @@ fn calc_threshold(pixels: &[u8], bpp: usize) -> u8{
     let dist = (right_max_pos as isize-left_max_pos as isize).abs(); //位置距离
     if dist<sigma as isize{//另寻低峰
         // (4) 求出像素灰度中值点位置
-        gray_values.sort();//排序并取中间值
-        let mid_value = gray_values[gray_values.len()/2];
+        let mut sort_gray_values = gray_values.clone();
+        sort_gray_values.sort();//排序并取中间值
+        let mid_value = sort_gray_values[sort_gray_values.len()/2];
 
         // (5) 如果midpos>avg表明小峰在大峰左边(较低灰度级)；否则，表明小峰在大峰右边(较高灰度级)，相应调整分界点位置
         // (6) 重新求出大、小峰的位置
-        if mid_value>avg{//小峰在大峰左边
+        if mid_value as i32>avg{//小峰在大峰左边
             //从原先左峰往左边移动sigma距离，寻找最高峰
             left_max_pos = gray_count.get(0..(left_max_pos-sigma as usize)).unwrap().iter().enumerate().max_by(|(_, v1), (_, v2)|{ v1.cmp(v2) }).unwrap().0;
         }else{//小峰在大峰右边
             //从原先右峰往右边移动sigma距离，寻找最高峰
             right_max_pos = gray_count.get((right_max_pos+sigma as usize)..gray_count.len()).unwrap().iter().enumerate().max_by(|(_, v1), (_, v2)|{ v1.cmp(v2) }).unwrap().0+(right_max_pos+sigma as usize);
         }
-        (left_max_pos + (right_max_pos-left_max_pos)/2) as u8
+        ((left_max_pos + (right_max_pos-left_max_pos)/2) as u8, gray_values)
     }else{
         // (7) 以两峰位置的中点做为阈值
-        (left_max_pos + (right_max_pos-left_max_pos)/2) as u8
+        ((left_max_pos + (right_max_pos-left_max_pos)/2) as u8, gray_values)
     }
 }
 
+/// 灰度图像边缘检测，填充为白底黑前景的RGB/RGBA图
+///
+/// # Params
+///
+/// - `gray_values`: 每个像素对应的灰度值
+/// - `out`: 输出RGB/RGBA(背景为白色，前景为黑色)
+/// - `bpl`: 每行占用字节
+/// - `bpp`: 每个像素占用字节
+/// - `thresholds`: 阈值
+pub fn edge_detect_gray(gray_values:&[u8], out:&mut [u8], bpl:usize, bpp: usize, threshold:u8){
+    // 检测格式:
+    //
+    //     B H
+    //     H
+    //
+    // B 当前像素 Bipolar cell 双极细胞
+    // H 周围像素 Horizontal cell 水平细胞
+    let width = bpl/bpp;
+    let pixels_count = gray_values.len();
+
+    //let mut i = 0;
+    for (i, pixel) in out.chunks_mut(bpp).enumerate(){
+        let hrid = i+1;//右边一个像素[水平细胞]的位置(向后偏移一个像素)
+        let hbid = i+width;//下边一个像素[水平细胞]位置(向后偏移一行)
+
+        //当前像素[双极细胞]输出     -- 亮光兴奋，弱光抑制
+        let b_out = if gray_values[i] >= threshold{ 1 }else{ -1 };
+        
+        //计算周围像素[水平细胞]输出  -- 亮光抑制，弱光兴奋
+        if hrid<pixels_count && hbid < pixels_count{
+            let hr_out = if gray_values[hrid] >= threshold{ -1 }else{ 1 };
+            let hb_out = if gray_values[hbid] >= threshold{ -1 }else{ 1 };
+
+            if b_out*2+hr_out+hb_out != 0{
+                //标记边缘
+                pixel[0] = 0;
+                pixel[1] = 0;
+                pixel[2] = 0;
+            }else{
+                pixel[0] = 255;
+                pixel[1] = 255;
+                pixel[2] = 255;
+            }
+            if bpp == 4{
+                pixel[3] = 255;//不透明
+            }
+        }
+    }
+}
 
 fn main(){
     // let img = image::open("lena.jpg").unwrap().to_rgb();
-    // let img = image::open("qq.jpg").unwrap().to_rgb();
+    let img = image::open("qq.jpg").unwrap().to_rgb();
     // let img = image::open("jt.png").unwrap().to_rgb();
     // let img = image::open("shou.png").unwrap().to_rgb();
     // let img = image::open("s.png").unwrap().to_rgb();
     // let img = image::open("aa.jpeg").unwrap().to_rgb();
-    let img = image::open("bb.jpg").unwrap().to_rgb();
+    // let img = image::open("bb.jpg").unwrap().to_rgb();
     let (width, height) = (img.width() as usize, img.height() as usize);
-    let pixels = img.into_raw();
+    let mut pixels = img.into_raw();
 
     let now = Instant::now();
     
-    let t = calc_threshold(&pixels, 3);
+    // let mut t = 0;
+    // for _ in 0..1000{
+    //     t = calc_threshold(&pixels, 3).0;
+    // }
+    let bpp = 3;
+
+    //计算阈值和像素灰度值
+    let (t, gray_values) = calc_threshold(&pixels, bpp);
+    //通过边缘检测，将图片转换为白底黑字
+    edge_detect_gray(&gray_values, &mut pixels, bpp*width, bpp, t);
+    //切割黑色像素
+
+    //从左往右，水平方向投影扫描
+    let mut horizontal_shoots = vec![0; width];
+    for row in pixels.chunks(width*bpp){
+        for (x, pixel) in row.chunks(bpp).enumerate(){
+            if pixel[0] == 0{
+                horizontal_shoots[x] += 1;
+            }
+        }
+    }
+    println!("horizontal_shoots={:?}", horizontal_shoots);
+
+    //绘制投影图
+    {
+        let height = 50;
+        let mut shoot = ImageBuffer::new(width as u32, height);
+        let mut last_point = None;
+        for y in 0..height{
+            for x in 0..width{
+                if horizontal_shoots[x] == (height-y){
+                    if last_point.is_none(){
+                        last_point = Some((x as f32, y as f32));
+                    }else{
+                        imageproc::drawing::draw_line_segment_mut(&mut shoot, last_point.unwrap(), (x as f32, y as f32), Rgb([255u8, 255u8, 0u8]));
+                        last_point = Some((x as f32, y as f32));
+                    }
+                }
+            }
+        }
+        
+        shoot.save("shoot.png").unwrap();
+    }
+
+    //切割结果处理: 根据切合图片的比例，正方形按照单个识别，横向长方形按照单行识别，纵向长方形不识别。
+
 
     println!("耗时{}ms 阈值:{}", duration_to_milis(&now.elapsed()), t);
-
-    //--------------------------------- 灰度直方图
-    // let height = 100;
-    // let width = 256;
-    // let mut gray_bar = vec![255; width*height*3];
-    // let max = *gray_count.iter().max().unwrap();
-    // for (y, row) in gray_bar.chunks_mut(width*3).enumerate(){
-    //     for (x, pixel) in row.chunks_mut(3).enumerate(){
-    //         let h = ((gray_count[x] as f32/max as f32)*height as f32) as usize;
-    //         if h>=(height-y){
-    //             pixel[0] = 0;
-    //             pixel[1] = 0;
-    //             pixel[2] = 0;
-    //         }
-    //     }
-    // }
-
-    //println!("{:?} max={}", gray_count, max);
-
-    //let newimg:ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::from_raw(new_width as u32, new_height as u32, dst).unwrap();
-    // let newimg:ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::from_raw(width as u32, height as u32, gray_bar).unwrap();
-    // println!("width={}", newimg.width());
-    // newimg.save("new.png").unwrap();
-    return;
-
-    
-    let mut out = vec![255; width*height*3];
-    retina::edge_detect(width, 3, &pixels, &mut out, 63);
-    retina::edge_detect(width, 3, &pixels, &mut out, 97);
-
-    println!("耗时{}ms", duration_to_milis(&now.elapsed()));
 
     //分割算法
     //https://blog.csdn.net/zhongyunde/article/details/7840717
     
-
-    let newimg:ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::from_raw(width as u32, height as u32, out).unwrap();
-    
-    println!("width={}", newimg.width());
+    let newimg:ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::from_raw(width as u32, height as u32, pixels).unwrap();
     newimg.save("new.png").unwrap();
-    
 }
 
 pub fn duration_to_milis(duration: &Duration) -> f64 {
