c42f8a9e62f527a15a5ef7ef0d9bc8a3f8e642d4
planet0104 <planet0104@gmail.com>
2018-10-31 18:11:31 +0800

    capture

--- lazy_dict/app/src/main/java/cn/jy/lazydict/CameraActivity.java
+++ lazy_dict/app/src/main/java/cn/jy/lazydict/CameraActivity.java
@@ -1,42 +1,67 @@
 package cn.jy.lazydict;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.DashPathEffect;
 import android.graphics.ImageFormat;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class CameraActivity extends Activity implements SurfaceHolder.Callback{
+import java.util.ArrayList;
+import java.util.List;
+
+public class CameraActivity extends Activity{
     static final String TAG = CameraActivity.class.getSimpleName();
+
+    //--------- 预览相关 ------------
+    FrameLayout fl_preview;
     SurfaceView surface_view;
     SurfaceHolder surface_holder;
-    Camera camera;
-    CaptureView iv_capture;
     ImageButton btn_capture;
+
+    //------ 截图相关 -------------
+    FrameLayout fl_capture;
+    ImageView iv_mask;
+    ImageView iv_capture;
     ImageButton btn_preview;
-    public static ImageView iv_test;
-    Bitmap captureBitmap;
+
+    //所有用户选择的Rect
+    List<RectF> allRects = new ArrayList<>();
+    Bitmap mask;
+    Bitmap capture;
+    Camera camera;
     byte[] previewFrame = null;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
         iv_capture = findViewById(R.id.iv_capture);
+        iv_mask = findViewById(R.id.iv_mask);
+        fl_capture = findViewById(R.id.fl_capture);
+        iv_capture.setDrawingCacheEnabled(true);
+
         btn_capture = findViewById(R.id.btn_capture);
         btn_preview = findViewById(R.id.btn_preview);
         surface_view = findViewById(R.id.surface_view);
-        iv_test = findViewById(R.id.iv_test);
         surface_holder = surface_view.getHolder();
-        surface_holder.addCallback(this);
-
         btn_capture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,6 +75,36 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback{
                 if(camera == null) initCamera();
             }
         });
+
+        iv_mask.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        addRect(event.getX(), event.getY());
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    /**
+     * 切换到预览视图
+     */
+    private void changeStatePreview(){
+        Log.d(TAG, "changeStatePreview");
+        fl_capture.setVisibility(View.GONE);
+        fl_preview.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * 切换到截屏视图
+     */
+    private void changeStateCapture(){
+        Log.d(TAG, "changeStateCapture");
+        fl_capture.setVisibility(View.VISIBLE);
+        fl_preview.setVisibility(View.GONE);
     }
 
     private void capture(){
@@ -58,21 +113,28 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback{
             android.hardware.Camera.CameraInfo info =
                     new android.hardware.Camera.CameraInfo();
             android.hardware.Camera.getCameraInfo(Camera.CameraInfo.CAMERA_FACING_BACK, info);
-            
+
             Camera.Size size = camera.getParameters().getPreviewSize();
-            int[] colors = RustApp.decodeYUV420SP(previewFrame, size.width, size.height, info.orientation);
-            iv_capture.setImageBitmap(Bitmap.createBitmap(colors,0, size.width, size.width, size.height, Bitmap.Config.ARGB_8888));
-            btn_capture.setVisibility(View.GONE);
-            iv_capture.setVisibility(View.VISIBLE);
-            btn_preview.setVisibility(View.VISIBLE);
-            releaseCamera();//释放相机
+            long t = System.currentTimeMillis();
+            try {
+                capture = Toolkit.decodeYUV420SP(previewFrame, size.width, size.height, info.orientation);
+                iv_capture.setImageBitmap(capture);
+                Log.d(TAG, "转换耗时:"+(System.currentTimeMillis()-t)+"ms");
+                releaseCamera();//释放相机
+                changeStateCapture();//切换到截图状态
+            } catch (Exception e) {
+                e.printStackTrace();
+                toast(this, e.getMessage());
+            }
         }
     }
 
+    /**
+     * 关闭相机
+     */
     private void releaseCamera(){
         Log.d(TAG, "releaseCamera.");
         if (camera != null) {
-            surface_holder.removeCallback(this);
             camera.setPreviewCallback(null);
             camera.stopPreview();
             camera.lock();
@@ -81,6 +143,9 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback{
         }
     }
 
+    /**
+     * 启动相机
+     */
     private void initCamera(){
         Log.d(TAG, "initCamera.");
         camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);//默认开启后置
@@ -97,10 +162,8 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback{
                         previewFrame = data;
                     }
                 });
+                changeStatePreview();
                 camera.startPreview();
-                btn_preview.setVisibility(View.GONE);
-                iv_capture.setVisibility(View.GONE);
-                btn_capture.setVisibility(View.VISIBLE);
             }catch (Exception e) {
                 releaseCamera();
             }
@@ -108,21 +171,67 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback{
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        Log.d(TAG, "surfaceCreated.");
-        //初始化Camera
-        initCamera();
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        Log.d(TAG, "surfaceDestroyed.");
-        //释放Camera
+    protected void onStop() {
         releaseCamera();
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //使用post, 解决Camera: app passed NULL surface
+        surface_view.post(new Runnable() {
+            @Override
+            public void run() {
+                if(camera==null && iv_capture.getVisibility()==View.GONE){
+                    initCamera();
+                }
+            }
+        });
+    }
+
+    public static void toast(Context context, String s){
+        Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
+    }
+
+    private void addRect(float x, float y){
+        Bitmap bitmap = iv_capture.getDrawingCache();
+        try {
+            long t = System.currentTimeMillis();
+            RectF rect = Toolkit.getCharacterRect(bitmap, (int)x, (int)y);
+            Log.d(TAG, "x="+x+" y="+y+" bitmap.width="+bitmap.getWidth()+" rect="+rect.toShortString()+" 耗时:"+(System.currentTimeMillis()-t)+"ms");
+
+            //已经存在不再添加
+            for(RectF ur: allRects){
+                if(ur.left == rect.left && ur.top == rect.top && ur.right == rect.right && ur.bottom == rect.bottom){
+                    toast(this, "已经添加!");
+                    return;
+                }
+            }
+
+            allRects.add(rect);
+
+            if(mask == null){
+                mask = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+            }
+
+            Canvas canvas = new Canvas(mask);
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(Toolkit.dip2px(this,5));
+
+            for(RectF ur: allRects){
+                paint.setPathEffect(null);
+                paint.setColor(0x7fff0000);
+                canvas.drawRoundRect(rect, Toolkit.dip2px(this,5), Toolkit.dip2px(this,5), paint);
+                paint.setColor(0x7fffffff);
+                paint.setPathEffect(new DashPathEffect(new float[]{Toolkit.dip2px(this,10), Toolkit.dip2px(this,10)}, 0));
+                canvas.drawRoundRect(ur, Toolkit.dip2px(this,5), Toolkit.dip2px(this,5), paint);
+            }
+            iv_mask.setImageBitmap(mask);
+        } catch (Exception e) {
+            e.printStackTrace();
+            CameraActivity.toast(this, e.getMessage());
+        }
     }
 }
--- lazy_dict/app/src/main/java/cn/jy/lazydict/CaptureView.java
+++ lazy_dict/app/src/main/java/cn/jy/lazydict/CaptureView.java
@@ -2,11 +2,17 @@ package cn.jy.lazydict;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
-public class CaptureView extends android.support.v7.widget.AppCompatImageView {
+public class CaptureView extends ImageView {
     static final String TAG = CaptureView.class.getSimpleName();
 
     public CaptureView(Context context) {
@@ -39,6 +45,30 @@ public class CaptureView extends android.support.v7.widget.AppCompatImageView {
 
     private void addCharacter(float x, float y){
         Bitmap bitmap = getDrawingCache();
-        Log.d(TAG, "x="+x+" y="+y+" bitmap.width="+bitmap.getWidth());
+        try {
+            long t = System.currentTimeMillis();
+            RectF rect = Toolkit.getCharacterRect(bitmap, (int)x, (int)y);
+            Log.d(TAG, "x="+x+" y="+y+" bitmap.width="+bitmap.getWidth()+" rect="+rect.toShortString()+" 耗时:"+(System.currentTimeMillis()-t)+"ms");
+            Canvas canvas = new Canvas(bitmap);
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(dip2px(5));
+            paint.setColor(0x7fff0000);
+            canvas.drawRoundRect(rect, dip2px(5), dip2px(5), paint);
+            paint.setColor(0x7fffffff);
+            paint.setPathEffect(new DashPathEffect(new float[]{dip2px(10), dip2px(10)}, 0));
+            canvas.drawRoundRect(rect, dip2px(5), dip2px(5), paint);
+            setImageBitmap(bitmap);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            CameraActivity.toast(getContext(), e.getMessage());
+        }
+    }
+
+    public int dip2px(float dipValue){
+        final float scale = getContext().getResources().getDisplayMetrics().density;
+        return (int)(dipValue * scale + 0.5f);
     }
 }
