ba6a381d0b23f0538191b247bd532b229f39427b
planet0104 <planet0104@gmail.com>
2018-11-08 18:09:42 +0800

    bd

--- lazy_dict/app/src/androidTest/java/com/test/sdl/sdl2test/ExampleInstrumentedTest.java
+++ lazy_dict/app/src/androidTest/java/com/test/sdl/sdl2test/ExampleInstrumentedTest.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 /**
- * Instrumented test, which will execute on an Android device.
+ * Instrumented bg_ring, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
@@ -18,9 +18,9 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
+        // Context of the app under bg_ring.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.test.sdl.sdl2test", appContext.getPackageName());
+        assertEquals("com.bg_ring.sdl.sdl2test", appContext.getPackageName());
     }
 }
